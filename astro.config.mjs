eb927edc1f8bc2d4fe2e690c0681336c0eb5d94b
akicookz <akbar@As-MacBook-Pro.local>
2025-04-15 15:47:25 +0900

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  site: "https://questinstoaskyourself.com",
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
